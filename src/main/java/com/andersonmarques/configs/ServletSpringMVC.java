7e29d432e412fd5072bb98b941107a911b91a5dc
AndersonMarquess <andersongabriel96.ag@gmail.com>
2018-09-21 21:26:56 -0300

    Configurado profile inicial do sistema

--- src/main/java/com/andersonmarques/configs/ServletSpringMVC.java
+++ src/main/java/com/andersonmarques/configs/ServletSpringMVC.java
@@ -2,8 +2,11 @@ package com.andersonmarques.configs;
 
 import javax.servlet.Filter;
 import javax.servlet.MultipartConfigElement;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;
 
+import org.springframework.web.context.request.RequestContextListener;
 import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
@@ -45,5 +48,13 @@ public class ServletSpringMVC extends AbstractAnnotationConfigDispatcherServletI
 	protected void customizeRegistration(Dynamic registration) {
 		registration.setMultipartConfig(new MultipartConfigElement(""));
 		
+	}
+	
+	/* Configura o profile ativo inicial da aplicação */
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		super.onStartup(servletContext);
+		servletContext.addListener(RequestContextListener.class);
+		servletContext.setInitParameter("spring.profiles.active", "dev");//dev é o profile do banco de dados normal
 	}
 }
